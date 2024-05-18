66c62ff84ce7049e5a36a6e8624886e3bec662a6
Viviancat666 <vivian9069@gmail.com>
2024-05-18 21:03:21 +0300

    Create 20.sql

---
+++ 04_basic_crud_exercise/20.sql
@@ -0,0 +1,8 @@
+CREATE VIEW view_addresses AS
+SELECT concat(e.first_name, ' ', e.last_name) as full_name,
+       e.department_id,
+       concat(a.number, ' ', a.street)        AS address
+FROM employees AS e
+         JOIN addresses as a on e.address_id = a.id
+ORDER BY address
+;
