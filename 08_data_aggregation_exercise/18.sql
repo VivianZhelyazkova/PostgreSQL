18cc22ac08ed3e19c1f1e20ebd980cbf30d81f6f
Viviancat666 <vivian9069@gmail.com>
2024-05-27 20:24:38 +0300

    Create 18.sql

---
+++ 08_data_aggregation_exercise/18.sql
@@ -0,0 +1,16 @@
+CREATE VIEW view_performance_rating AS
+SELECT
+       first_name,
+       last_name,
+       job_title,
+       salary,
+       department_id,
+       CASE
+           WHEN salary >= 25000 THEN
+               CASE
+                WHEN job_title LIKE 'Senior%' THEN 'High-performing Senior'
+                WHEN job_title  NOT LIKE 'Senior%' THEN 'High-performing Employee'
+                END
+           ELSE 'Average-performing'
+       END AS performance_rating
+FROM employees;
