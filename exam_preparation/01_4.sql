e0a042a99a3c37dc5200ae9a5c3489e8b8054225
Viviancat666 <vivian9069@gmail.com>
2024-06-10 21:35:18 +0300

    Create 01_4.sql

---
+++ exam_preparation/01_4.sql
@@ -0,0 +1,8 @@
+DELETE
+FROM volunteers
+WHERE department_id = (SELECT id FROM volunteers_departments WHERE department_name = 'Education program assistant');
+
+DELETE
+FROM volunteers_departments
+WHERE department_name = 'Education program assistant';
+
