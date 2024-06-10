1c0c12405705106c0332267723b3849f3a88c028
Viviancat666 <vivian9069@gmail.com>
2024-06-10 21:35:11 +0300

    Create 4.sql

---
+++ exam_preparation/4.sql
@@ -0,0 +1,10 @@
+SELECT
+    o.name as owner,
+    COUNT(*) AS count_of_animals
+FROM owners AS o
+         JOIN animals AS a
+              ON o.id = a.owner_id
+GROUP BY o.name
+ORDER BY count_of_animals DESC ,owner
+LIMIT 5
+;
